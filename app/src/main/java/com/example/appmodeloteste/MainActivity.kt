ed9317ec5e6d5a61f2252f4a8027e0fbc57865ec
Matheus Alejandro <matheus.alejandro@oracle.com>
2021-08-16 22:52:47 -0300

    App Splash + tela de finish

--- app/src/main/java/com/example/appmodeloteste/MainActivity.kt
+++ app/src/main/java/com/example/appmodeloteste/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.appmodeloteste
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun acaoSair(view: View) {
+        Toast.makeText(this, "Sair do app!", Toast.LENGTH_SHORT).show()
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/appmodeloteste/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.appmodeloteste
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //avisa que vai chamar uma função após um delay
+        Handler(Looper.myLooper()!!).postDelayed({
+            //defini daonde eu estou (this = Splash) e para onde eu vou (MainActivity)
+            val intent = Intent(
+                this,
+                MainActivity::class.java
+            )
+
+            //manda rodar
+            startActivity(intent)
+            //para não retornar a pagina splash
+            finish()
+
+        }, 2000)
+    }
+}
+
