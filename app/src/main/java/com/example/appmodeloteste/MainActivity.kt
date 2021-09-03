2a9bb3f3f36a3b4862352721684047c93f3ccbf2
Matheus Alejandro <matheus.alejandro@oracle.com>
2021-09-03 16:07:24 -0300

    CodeStyle e Log

--- app/src/main/java/com/example/appmodeloteste/MainActivity.kt
+++ app/src/main/java/com/example/appmodeloteste/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.appmodeloteste
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 
@@ -12,6 +13,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun acaoSair(view: View) {
+        //TODO: criar todo
+        //FIXME: ação de fix
+        Log.i("INFORMATION", "Log de information")
+        Log.w("WARNING","Log de warning")
+        Log.e("ERROR","Log de erro")
+
+
         Toast.makeText(this, "Sair do app!", Toast.LENGTH_SHORT).show()
         finish()
     }
